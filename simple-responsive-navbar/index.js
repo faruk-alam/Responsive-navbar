52d3fd8e2c414f8d9191fbaecc0456504ccf7637
faruk-alam <farukinnfo@gmail.com>
2025-05-27 14:42:25 +0200

    simple navbar updated

--- simple-responsive-navbar/index.js
+++ simple-responsive-navbar/index.js
@@ -1,7 +1,22 @@
 const hamburgerMenu = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-links');
-console.log(hamburgerMenu);
-hamburgerMenu.addEventListener('click', () => {
+const navbarLinks = document.querySelectorAll('.links');
+const menuOverlay = document.querySelector('.menu-overlay');
+menuOverlay.classList.add('nav-links');
+menuOverlay.addEventListener('click', toggleMenu);
+hamburgerMenu.addEventListener('click', toggleMenu);
+function toggleMenu(){
     navMenu.classList.toggle('active');
     hamburgerMenu.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+}
+// Close navbar when clicking on links
+navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if(navMenu.classList.contains('active')) {
+            toggleMenu();
+        }
+        // navMenu.classList.remove('active');
+        // hamburgerMenu.classList.remove('active');
+    })
 })
